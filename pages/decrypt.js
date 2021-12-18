ae84039da22ead1e763266fd53bda5f007893189
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2021-12-19 01:23:47 +0530

    init setup

--- pages/decrypt.js
+++ pages/decrypt.js
@@ -1,32 +1,29 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { AES, enc } from 'crypto-js';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
-  const [link , setLink] = useState(null)
+  const finalRef = useRef(null);
   const [pass , setPass] = useState(null)
   const [encrypted, setEncrypted] = useState(null);
-
   const router = useRouter();
+  useEffect(()=>{
+      setEncrypted(decodeURIComponent(router.query.enc));
+  }, [router.query.enc]);
   return (
    <>
     <h1>Nara Link Locker</h1>
-    <input type={"text"} 
-    onChange={e => setLink(e.target.value)} 
-    value={link} 
-    placeholder='Enter your link'/>
     <input type={"password"} 
     onChange={e => setPass(e.target.value)} 
-    value={pass} 
+    value={pass}
     placeholder='Enter your password'/>
     <button onClick={()=>{
-        router.push({
-          pathname:"/decrypt",
-          query:{enc:encodeURIComponent(AES.encrypt(link, pass).toString())}
-        })
-    }}>Encrypt</button>
+        setEncrypted(AES.decrypt(encrypted, pass).toString(enc.Utf8));
+    }}>Decrypt</button>
     <br/>
+    <span>{pass}</span>
+    <br/><span>{encrypted || "nice"}</span>
    </>
   )
 }
