9c8c9ef738bc787ce9fb624afbf5c7b7d622093d
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2021-12-19 21:30:03 +0530

    index page styling

--- pages/decrypt.js
+++ pages/decrypt.js
@@ -5,25 +5,25 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
   const finalRef = useRef(null);
-  const [pass , setPass] = useState(null)
+  const [pass, setPass] = useState(null)
   const [encrypted, setEncrypted] = useState(null);
   const router = useRouter();
-  useEffect(()=>{
-      setEncrypted(decodeURIComponent(router.query.enc));
+  useEffect(() => {
+    setEncrypted(decodeURIComponent(router.query.enc));
   }, [router.query.enc]);
   return (
-   <>
-    <h1>Nara Link Locker</h1>
-    <input type={"password"} 
-    onChange={e => setPass(e.target.value)} 
-    value={pass}
-    placeholder='Enter your password'/>
-    <button onClick={()=>{
+    <>
+    <div className="flex flex-col">
+    </div>
+      <input type={"password"}
+        onChange={e => setPass(e.target.value)}
+        value={pass}
+        placeholder='Enter your password' />
+      <button onClick={() => {
         setEncrypted(AES.decrypt(encrypted, pass).toString(enc.Utf8));
-    }}>Decrypt</button>
-    <br/>
-    <span>{pass}</span>
-    <br/><span>{encrypted || "nice"}</span>
-   </>
+      }}>Decrypt</button>
+      <span>{pass}</span>
+      <span>{encrypted || "nice"}</span>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,54 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { AES, enc } from 'crypto-js';
-import { useRef, useState } from 'react';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { AES, enc } from "crypto-js";
+import { useRef, useState } from "react";
 
 export default function Home() {
-  const [link , setLink] = useState(null)
-  const [pass , setPass] = useState(null)
+  const [link, setLink] = useState(null);
+  const [pass, setPass] = useState(null);
   const [encrypted, setEncrypted] = useState(null);
-
   const router = useRouter();
+  const handler = () => {
+    router.push({
+      pathname: "/decrypt",
+      query: {
+        enc: encodeURIComponent(AES.encrypt(link, pass).toString())
+      }
+    });
+  };
   return (
-   <>
-    <h1>Nara Link Locker</h1>
-    <input type={"text"} 
-    onChange={e => setLink(e.target.value)} 
-    value={link} 
-    placeholder='Enter your link'/>
-    <input type={"password"} 
-    onChange={e => setPass(e.target.value)} 
-    value={pass} 
-    placeholder='Enter your password'/>
-    <button onClick={()=>{
-        router.push({
-          pathname:"/decrypt",
-          query:{enc:encodeURIComponent(AES.encrypt(link, pass).toString())}
-        })
-    }}>Encrypt</button>
-    <br/>
-   </>
-  )
+    <>
+      <div></div>
+      <section className="flex flex-col justify-center items-center gap-10">
+        <div className="text-center">
+          <h2 className="font-bold text-4xl">
+            Password Protect your Links with Nara
+          </h2>
+          <p>
+            Enter your Link and Password in their respective fields, then press
+            Enter.
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-3 w-48">
+          <input
+            type={"text"}
+            onChange={(e) => setLink(e.target.value)}
+            value={link}
+            placeholder="🔗 Enter your link ⭾"
+            className="bg-slate-500 text-white rounded-full p-3 placeholder:text-white duration-150 indent-2"
+          />
+          <input
+            type={"password"}
+            onChange={(e) => setPass(e.target.value)}
+            value={pass}
+            className="bg-slate-500 text-white rounded-full p-3 placeholder:text-white duration-150 indent-2"
+            placeholder="🔒 Enter your password ⏎"
+            onKeyPress={(e) => {
+              e.key === "Enter" && handler();
+            }}
+          />
+        </div>
+      </section>
+    </>
+  );
 }
